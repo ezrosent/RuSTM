50e89b531e63a6cea8bc3d7cb8c054e9f4295dc9
John Ericson <john.ericson@delphix.com>
2015-07-24 01:30:39 -0700

    Misc fixes and style warning appeasement

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod STM {
+pub mod stm {
     use std::result::Result as Either;
     use std::mem::{transmute, forget, uninitialized};
     use std::ptr::swap;
@@ -15,7 +15,7 @@ mod STM {
         }
 
         /// just don't look
-        pub fn spec_move(mut self) -> Either<A, ()> {
+        fn spec_move(mut self) -> Either<A, ()> {
             unsafe {
                 let mut x = uninitialized();
                 swap(&mut x, transmute(&mut self));
@@ -36,57 +36,57 @@ mod STM {
         }
     }
 
-    trait Stm {
+    pub trait GlobalState {
         /// A hook for transaction-local state
         type LocalState;
 
         fn run<A, F>(&self, F) -> A
-            where F: FnMut(&mut Self::LocalState) -> Result<A>,
-                  Self::LocalState: StmHelper<A>;
+            where F: FnMut(&mut Self::LocalState) -> Result<A>;
 
         fn or_else<A, F, G>(&self, F, G) -> Result<A>
             where F: FnMut(&mut Self::LocalState) -> Result<A>,
-                  G: FnMut(&mut Self::LocalState) -> Result<A>,
-                  Self::LocalState: StmHelper<A>;
+                  G: FnMut(&mut Self::LocalState) -> Result<A>;
     }
 
-    trait StmHelper<A> {
+    pub trait TVar<A> {
         /// A wrapper around an object whose access is mediated by the STM implementation
-        type TVar;
+        type LocalState;
 
-        /// Read/Write need to return Result<A>
-        fn read(&mut self, Self::TVar) -> A;
+        fn read(&mut self, Self::LocalState) -> Result<A>;
 
-        fn write(&mut self, Self::TVar, A);
+        fn write(&mut self, Self::LocalState, A) -> Result<()>;
     }
 }
-mod NoRec {
+pub mod no_rec {
     use std::sync::atomic::{AtomicUsize, Ordering};
     use std::vec::Vec;
     use std::collections::HashMap;
     use std::cell::UnsafeCell;
     use std::marker::PhantomData;
-    use std::mem::{transmute, forget, uninitialized};
-    use STM;
+    use stm::{self, Result};
 
-    struct GlobalState {
+    pub struct GlobalState {
         version : AtomicUsize
     }
 
-    #[derive(Hash, Eq, PartialEq, Debug, Clone)]
+    #[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
     /// Lies... Lies
     struct TVarAddr<'a, 'b : 'a>(*mut TVar<'b, ()>, PhantomData<&'a TVar<'b, ()>>);
 
+    impl<'a, 'b : 'a> TVarAddr<'a, 'b> {
+        fn version(self) -> &'a AtomicUsize {
+            unsafe { &(*self.0).version }
+        }
+    }
 
-    struct LocalState<'v, 'g : 'v> {
+    pub struct LocalState<'v, 'g : 'v> {
         /// the norec paper has the reads stored as a list of <address, value> pairs. For us it
         /// would probably be better to have versioned TVars. That way we always have a fast
         /// comparison operation and avoid doing extra dynamic dispatch when validating reads.
         reads : Vec<(TVarAddr<'v, 'g>, usize)>,
 
         /// for writes we will probably have to cast unsafely to get this to work. TVars just hold
-        /// arbitrary types and we only have one table.
-        /// XXX: gross
+        /// arbitrary types and we only have one table.  XXX: gross
         writes : HashMap<TVarAddr<'v, 'g>, Box<[u8]>>,
 
         /// STM environment we are using
@@ -107,37 +107,42 @@ mod NoRec {
          *          TXAbort() // abort will longjmp if (time == global lock)
          * return time
          */
-        fn validate(&mut self, glob : &'g GlobalState) -> STM::Result<()> {
+        fn validate(&mut self) -> Result<()> {
             loop {
-                let time = glob.version.load(Ordering::SeqCst);
+                let time = self.global.version.load(Ordering::SeqCst);
                 if (time & 1) == 0 {
                     continue;
                 }
-                for &(ref addr, ref version) in self.reads.iter() {
+                for &(addr, version) in &self.reads {
                     unsafe {
-                        let v = addr.0.clone();
-                        if (*v).version.load(Ordering::SeqCst) != *version {
-                            return STM::Result::abort();
+                        if addr.version().load(Ordering::SeqCst) != version {
+                            return Result::abort();
                         }
                     }
                 }
-                return STM::Result::ret(());
+                return Result::ret(());
             }
         }
     }
 
-
-    struct TVar<'a,A> {
+    pub struct TVar<'a, A> {
         version : AtomicUsize,
         val : UnsafeCell<A>,
         global : &'a GlobalState
     }
 
     impl<'a, A> TVar<'a, A> {
-        unsafe fn get_val(&self) -> &mut A {
+        unsafe fn get_val(&self) -> &A {
+            &*self.val.get()
+        }
+
+        unsafe fn get_val_mut(&self) -> &mut A {
             &mut *self.val.get()
         }
     }
+
+    //impl<'a, A> stm::TVar<A> for TVar<'a, A> {
+    //}
 }
 
 #[test]
